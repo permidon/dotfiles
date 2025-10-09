cbfa470c4fa373977a23aad9b16388df0ae405a3
Roman Perminov <permidon@me.com>
2025-10-09 11:48:05 -0400

    enable linehl and blame

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,4 +2,8 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = true, -- uses default config
+  opts = {
+    current_line_blame = true,
+    linehl = true,
+  },
 }
